9611b6eeef805568fd240d3c392b2707bfe81ac3
NatureZhang <895234387@qq.com>
2016-04-19 23:13:18 +0800

    Initial Commit

---
+++ NSTimer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSTimer
+//
+//  Created by 张东 on 16/4/19.
+//  Copyright © 2016年 __Nature__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
